20675e50367548e77a316eab9fe08d657708ff32
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2023-03-02 08:38:36 +0000

    Update my-scriptlets.js

--- other lists/my-scriptlets.js
+++ other lists/my-scriptlets.js
@@ -5,19 +5,7 @@
 // example.com##+js(cs, name, value, age)
 (() => {
     'use strict';
-    const cs = ev => {
-        if (ev) {
-            window.removeEventListener(ev.type, cs, true);
-        }
-        try {
-            document.cookie = '{{1}}={{2}}; max-age={{3}}; secure; path=/;';
-        } catch { }
-    };
-    if (document.readyState === 'loading') {
-        window.addEventListener('DOMContentLoaded', cs, true);
-    } else {
-        cs();
-    }
+    document.cookie = "{{1}}={{2}}; max-age={{3}}; secure; path=/;";
 })();
 
 // https://github.com/uBlock-user/uBO-Scriptlets/commit/3d1f48573749ac85b20031f78e0d5f7c7bb0f3af#
@@ -48,10 +36,10 @@
 })();
 
 // based on https://github.com/NanoAdblocker/NanoFilters/blob/master/NanoFilters/NanoResources.txt#L283
-// You can optional set a timeout in milliseconds.
+// You can optional set a timeout in milliseconds before it clicks.
 // If not set, it will click directly.
-// example.com##+js(ce,#badbutton)
-// example.com##+js(ce,#badbutton,1000)
+// example.com##+js(ce, element)
+// example.com##+js(ce, element, timeout)
 /// click-element.js
 /// alias ce.js
 (() => {
@@ -86,6 +74,8 @@
 // Taken from AdGuard
 /// click-element-observer.js
 /// alias ceo.js
+// example.com##+js(ceo, element)
+// example.com##+js(ceo, element, disconnectTimeout)
 (() => {
     let a = '{{1}}';
     if ( a === '' || a === '{{1}}' ) {
