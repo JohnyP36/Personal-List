c793efd74ed4fe49aa3537601fa3722268093a4a
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2023-03-02 18:32:42 +0000

    Update my-scriptlets.js

--- other lists/my-scriptlets.js
+++ other lists/my-scriptlets.js
@@ -2,48 +2,76 @@
 /// setCookie.js
 /// alias sc.js
 /// alias cs.js
-// example.com##+js(cs, name, value, age)
+// example.com##+js(cs, name, value, age, domain, path, SameSite)
+// name and value are required, the others are options
 (() => {
     'use strict';
     const cs = ev => {
-        if (ev) {
-            window.removeEventListener(ev.type, cs, true);
-        }
+        if (ev) { window.removeEventListener(ev.type, cs, true); }
         try {
-            document.cookie = '{{1}}={{2}}; max-age={{3}}; secure; path=/;';
+            document.cookie = '{{1}}={{2}}; max-age={{3}}; domain={{4}}; path={{5}}; SameSite={{6}}; secure;';
         } catch { }
     };
-    if (document.readyState === 'loading') {
+    if ( document.readyState === 'loading' ) {
         window.addEventListener('DOMContentLoaded', cs, true);
     } else {
         cs();
     }
 })();
 
-// https://github.com/uBlock-user/uBO-Scriptlets/commit/3d1f48573749ac85b20031f78e0d5f7c7bb0f3af#
+// https://github.com/uBlock-user/uBO-Scriptlets/blob/master/scriptlets.js
 /// setLocalStorageItem.js
 /// alias slsi.js
-/// alias si.js
-// example.com##+js(si, key, value)
+/// alias sli.js
+// example.com##+js(sli, key, value)
 (() => {
     'use strict';
     const key = '{{1}}';
-    if ( key === '' || key === '{{1}}' ) { return; }
-    const value = '{{2}}';
-    if ( value === '' || value === '{{2}}' ) { return; }
-    const setItem = ev => {
-        if (ev) {
-            window.removeEventListener(ev.type, setItem, true);
-        }
-        try {
-            if (localStorage.getItem(key) !== null) { return; }
-            localStorage.setItem(key, value);
-        } catch { }
-    };
-    if (document.readyState === 'loading') {
-        window.addEventListener('DOMContentLoaded', setItem, true); 
-    } else {
-        setItem();
+        if ( key === '' || key === '{{1}}' ) { return; }
+        const keys = key.split(/\s*\|\s*/);
+        const value = '{{2}}';
+        const behavior = '{{3}}';
+        const setItem = () => {
+            let timer = undefined;
+            try {
+                for (const keyName of keys) {
+                    if (localStorage.getItem(keyName) === value) { break; }
+                    localStorage.setItem(keyName, value);
+                 }
+            } catch { }
+        };
+        const mutationHandler = mutations => {
+            if ( timer !== undefined ) { return; }
+            let skip = true;
+            for ( let i = 0; i < mutations.length && skip; i++ ) {
+                const { type, addedNodes, removedNodes } = mutations[i];
+                if ( type === 'attributes' ) { skip = false; }
+                for ( let j = 0; j < addedNodes.length && skip; j++ ) {
+                    if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
+                }
+                for ( let j = 0; j < removedNodes.length && skip; j++ ) {
+                    if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
+                }
+            }
+            if ( skip ) { return; }
+            timer = self.requestIdleCallback(setItem, { timeout: 10 });
+        };
+        const start = ( ) => {
+            setItem();
+            if ( /\bloop\b/.test(behavior) === false ) { return; }
+            const observer = new MutationObserver(mutationHandler);
+            observer.observe(document.documentElement, {
+                attributes: true,
+                childList: true,
+                subtree: true,
+            });
+        };
+        if ( document.readyState !== 'complete' && /\bcomplete\b/.test(behavior) ) {
+            self.addEventListener('load', start, { once: true });
+        } else if ( document.readyState === 'loading' ) {
+            self.addEventListener('DOMContentLoaded', start, { once: true });
+        } else {
+            start();
     }
 })();
 
@@ -59,8 +87,16 @@
     if ( selector === '' || selector === '{{1}}' ) {
         return;
     }
-    let msecs = '{{2}}';
-    if ( msecs === '{{2}}' ) {
+    let href = '{{2}}';
+    if ( href === ''  || href === '{{2}}' ) {
+        href = '';
+    }
+    let cookie = '{{3}}';
+    if ( cookie === '' ) {
+        cookie = -1;
+    }
+    let msecs = '{{4}}';
+    if ( msecs === '{{4}}' ) {
         msecs = '';
     }
     let timeout = parseInt(msecs, 10);
@@ -73,40 +109,53 @@
             element.click();
         }
     };
-    setTimeout(function() {
-        if ( document.readyState === 'interactive' ||
-             document.readyState === 'complete' ) {
-            click();
-        } else {
-            addEventListener('DOMContentLoaded', click);
-        }
-    }, timeout);
+    if ( (window.location.href.indexOf(href) !== -1) && (document.cookie.indexOf(cookie) == -1) ) {
+        setTimeout(function() {
+            if ( document.readyState === 'interactive' ||
+                 document.readyState === 'complete' ) {
+                click();
+            } else {
+                addEventListener('DOMContentLoaded', click);
+            }
+        }, timeout);
+    }
 })();
 
 // Taken from AdGuard
 /// click-element-observer.js
 /// alias ceo.js
 // example.com##+js(ceo, element)
-// example.com##+js(ceo, element, disconnectTimeout)
+// example.com##+js(ceo, element, hrefIndex, cookie, disconnectTimeout)
+// 1: element to click; 2: url should match given token; 3: should not click when certain cookie is already set; 4: disconnectTimeout.
 (() => {
-    let a = '{{1}}';
-    if ( a === '' || a === '{{1}}' ) {
+    let aelem = '{{1}}';
+    if ( aelem === '' || aelem === '{{1}}' ) {
         return;
     }
-    let b = '{{2}}';
-    if ( b === '{{2}}' ) {
-        b = '';
+    let bhref = '{{2}}';
+    if ( bhref === ''  || bhref === '{{2}}' ) {
+        bhref = '';
     }
-    let c = parseInt(b, 10);
-    if ( isNaN(c) || isFinite(c) === false ) {
-        c = 10000;
+    let ccookie = '{{3}}';
+    if ( ccookie === '' ) {
+        ccookie = -1;
     }
-    const d = new MutationObserver(function() {
-        const e = document.querySelector(a);
-        e&&(d.disconnect(),e.click())
-    });
-    d.observe(document,{ childList:!0, subtree:!0 }),
-    setTimeout(function() {
-        e.disconnect()
-    }, c)
+    let dmsecs = '{{4}}';
+    if ( dmsecs === '{{4}}' ) {
+        dmsecs = '';
+    }
+    let etimeout = parseInt(dmsecs, 10);
+    if ( isNaN(etimeout) || isFinite(etimeout) === false ) {
+        etimeout = 10000;
+    }
+    if ( (window.location.href.indexOf(bhref) !== -1) && (document.cookie.indexOf(ccookie) == -1) ) {
+        const o = new MutationObserver(function() {
+            const e = document.querySelector(aelem);
+            e && (o.disconnect(), e.click())
+        });
+        o.observe(document,{ childList:!0, subtree:!0 }),
+        setTimeout(function() {
+            o.disconnect()
+        }, etimeout)
+    }
 })();
