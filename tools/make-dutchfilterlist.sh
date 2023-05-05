d57028b0c8a216d4f00f5545830279bb968f34c2
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2023-05-05 06:11:42 +0000

    Update make-dutchfilterlist.sh

--- tools/make-dutchfilterlist.sh
+++ tools/make-dutchfilterlist.sh
@@ -2,5 +2,5 @@
 #
 # This script assumes a linux environment
 
-echo "*** EasyDutch: Assembling EasyDutch.txt"
+echo "*** Ducth Filter List: Assembling Personal%20List%20(uBo).txt"
 node ./tools/make-list.js in=tools/dutch-filter-list.template out=Personal%20List%20(uBo).out.txt
