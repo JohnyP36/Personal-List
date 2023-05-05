00a2ed3e8d0bbcc1e7a997efec57578dcc7aac29
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2023-05-05 06:36:34 +0000

    Update make-dutchfilterlist.sh

--- tools/make-dutchfilterlist.sh
+++ tools/make-dutchfilterlist.sh
@@ -2,5 +2,5 @@
 #
 # This script assumes a linux environment
 
-echo "*** Ducth Filter List: Assembling Personal%20List%20(uBo).txt"
+echo "*** Dutch Filter List: Assembling Personal%20List%20(uBo).txt"
 node ./tools/make-list.js in=tools/dutch-filter-list.template out=Personal%20List%20(uBo).out.txt
